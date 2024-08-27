52826082b855f826d285895a8a98a7153753f760
tanaphat jaroenponvijit <13441@sai.ac.th>
2024-08-27 14:29:32 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,39 @@ export class HomePage {
 
   constructor() {}
 
+  items=[
+    {
+      id:1,
+      name:'Product A',
+      price: 200,
+      detail:'this is a product A',
+      imageURL: 'https://inwfile.com/s-f/ebpwqj.jpg',
+    },
+
+    {
+      id:1,
+      name:'Product B',
+      price: 250,
+      detail:'this is a product B',
+      imageURL: 'https://inwfile.com/s-ct/mlquvy.jpg',
+    },
+
+    {
+      id:1,
+      name:'Product C',
+      price: 300,
+      detail:'this is a product C',
+      imageURL: 'https://inwfile.com/s-ct/o2gdd7.jpg',
+    },
+
+    {
+      id:1,
+      name:'Product D',
+      price: 350,
+      detail:'this is a product D',
+      imageURL: 'https://inwfile.com/s-ct/36q5w1.jpg',
+    },
+  ]
+
+
 }
